f82faaed21c8398710c5f470f0fe689c7f923f53
Ahmad Shah <ah.seerat@gmail.com>
2022-11-14 12:59:16 -0500

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,17 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render 'birds/index.html.erb'
+    birds = Bird.all
+    # render plain: "Hello #{@birds[3].name}"
+    # render json: { hash_can_come_here: "this is a hash"}
+    # render json: ["as", "well", "as", "arrays"]
+    # render json: @birds # here we can directly render from controller but in json view
+    # render json: {birds: @birds, message: ['Hello Birds', 'Goodbye Birds']}
+    # render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }.to_json
+#  render json: @birds
+# render json: @birds
+# render json: @birds/
+render json: birds
+
+
   end
 end
